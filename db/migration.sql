862c92af32fc44a1064e1c126aff0ea86b4aee06
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2025-02-17 14:34:29 +0100

    Update database columns

--- db/migration.sql
+++ db/migration.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS visits (
-    visitor_id VARCHAR(16) NOT NULL,
-    page VARCHAR(1024) NOT NULL,
+    visitor_id VARCHAR(10) NOT NULL,
+    page VARCHAR(512) NOT NULL,
     date DATE NOT NULL,
     count BIGINT NOT NULL DEFAULT 0,
     PRIMARY KEY (visitor_id, page, date)
