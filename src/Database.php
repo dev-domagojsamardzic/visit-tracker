afa26639db02ba8212a5959ac3379fd934c8f39b
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2025-02-18 11:07:41 +0100

    Create Database class

---
+++ src/Database.php
@@ -0,0 +1,60 @@
+<?php
+
+class Database
+{
+
+    /**
+     * Host: docker service name: 'db'
+     * @var string
+     */
+    private string $host = 'db';
+    /**
+     * @var string
+     */
+    private string $database = 'tracker';
+    /**
+     * @var string
+     */
+    private string $username = 'tracker_user';
+    /**
+     * @var string
+     */
+    private string $password = 'tracker_password';
+    /**
+     * @var PDO|null
+     */
+    private ?PDO $connection;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        try {
+            $dsn = sprintf('mysql:host=%s;dbname=%s', $this->host, $this->database);
+            $connection = new \PDO($dsn, $this->username, $this->password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection = $connection;
+        } catch (PDOException $e) {
+            throw new Exception('Database connection failed. ' . $e->getMessage());
+        }
+    }
+
+    /**
+     * Execute a query with given parameters
+     * @param string $sql
+     * @param array $params
+     * @return PDOStatement
+     */
+    public function execute(string $sql, array $params = []): PDOStatement
+    {
+        $statement = $this->connection->prepare($sql);
+
+        foreach ($params as $index => $value) {
+            $statement->bindValue($index + 1, $value);
+        }
+
+        $statement->execute();
+        return $statement;
+    }
+}
