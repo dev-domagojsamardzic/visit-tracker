cefeee5d36904d615997fc279c9209fd49f5bd8d
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2025-02-18 18:01:44 +0100

    Create Table component class

---
+++ src/html/Table.php
@@ -0,0 +1,87 @@
+<?php
+
+class Table
+{
+    /**
+     * @var array
+     */
+    private array $data;
+    /**
+     * @var array
+     */
+    private array $headers;
+
+    /**
+     * @param array $data
+     * @param array $headers
+     */
+    public function __construct(array $data = [], array $headers = [])
+    {
+        $this->data = $data;
+        $this->headers = $headers;
+    }
+
+    /**
+     * Render the table
+     * @return string
+     */
+    public function render(): string
+    {
+        $html = '<table>';
+        $html .= $this->renderHeader();
+        $html .= $this->renderBody();
+        $html .= '</table>';
+        return $html;
+    }
+
+    /**
+     * Render table header
+     * @return string
+     */
+    private function renderHeader(): string
+    {
+        $header = "<thead><tr>";
+
+        foreach ($this->headers as $h) {
+            $header .= "<th>" . htmlspecialchars($h) . "</th>";
+        }
+
+        $header .= "</tr></thead>";
+
+        return $header;
+    }
+
+    /**
+     * Render table body
+     * @return string
+     */
+    private function renderBody(): string
+    {
+        if (count($this->data) === 0 ) {
+            return $this->renderEmptyState();
+        }
+
+        $body = "<tbody>";
+
+        foreach ($this->data as $row) {
+            $body .= "<tr>";
+            foreach ($row as $cell) {
+                $body .= "<td>" . htmlspecialchars($cell) . "</td>";
+            }
+            $body .= "</tr>";
+        }
+
+        $body .= "</tbody>";
+
+        return $body;
+    }
+
+    /**
+     * Render empty table state
+     * @return string
+     */
+    private function renderEmptyState(): string
+    {
+        return '<tbody><tr><td colspan="'.count($this->headers).'">No data found</td></tr></tbody>';
+    }
+}
