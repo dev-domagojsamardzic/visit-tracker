bbe4a30f269514012e8d399f43238f1cb6f77be9
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2025-02-18 11:05:17 +0100

    Remove count column

--- db/migration.sql
+++ db/migration.sql
@@ -2,6 +2,5 @@ CREATE TABLE IF NOT EXISTS visits (
     visitor_id VARCHAR(10) NOT NULL,
     page VARCHAR(512) NOT NULL,
     date DATE NOT NULL,
-    count BIGINT NOT NULL DEFAULT 1,
     PRIMARY KEY (visitor_id, page, date)
 );
