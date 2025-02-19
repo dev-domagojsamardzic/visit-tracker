9cb393191350b66de5dd94a47066f8f03c48355b
Domagoj Samardžić <dev.domagojsamardzic@gmail.com>
2025-02-19 16:38:46 +0100

    Add comment to migration file

--- db/migration.sql
+++ db/migration.sql
@@ -1,6 +1,8 @@
+# Composite primary key is added to ensure uniqe constraint
+# (date, page, visitor_id) order is because of query select ... where date ...
 CREATE TABLE IF NOT EXISTS visits (
-    visitor_id VARCHAR(10) NOT NULL,
+    visitor_id CHAR(10) NOT NULL,
     page VARCHAR(512) NOT NULL,
     date DATE NOT NULL,
-    PRIMARY KEY (visitor_id, page, date)
+    PRIMARY KEY (date, page, visitor_id)
 );
